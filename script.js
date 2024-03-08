d123cd3533114e09f2f105dee1bec56e81e5bb49
RayenMneja <rayenmneja2006@gmail.com>
2024-03-08 23:03:06 +0100

    test

--- script.js
+++ script.js
@@ -24,27 +24,28 @@ $(document).ready(function() {
                         // If username is not stored, prompt user to input their name
                         postElement.html(`
                         <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-left-radius: 10px;padding:5px;">
-                        <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-top-left-radius: 10px; border-bottom-right-radius: 10px;">${post.name} <i class="fa fa-check-double"></i></legend>
-                        </div>
+                        <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-top-left-radius: 10px; border-bottom-right-radius: 10px; float:right;" >${post.name} <i class="fa fa-check-double"></i></legend>
+                        
                         <div style="">
                         <strong>
                         <h3 style="color: #000;padding:5px;">${post.message} </h3>
-                        </strong>
+                        </strong></div>
                         </fieldset>
                     `);
                     } else {
                         // If username is stored, use it to send messages
                         postElement.html(`
                         <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-right-radius: 10px;padding:5px;">
-                        <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-top-left-radius: 10px; border-bottom-right-radius: 10px;">${post.name} <i class="fa fa-check-double"></i></legend>
-                        </div>
-                        <div style="">
+                        <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-bottom-left-radius: 10px; border-top-right-radius: 10px;position:absolute;">${post.name} <i class="fa fa-check-double"></i></legend>
+                        
+                        <div style="margin-top:30px">
                         <strong>
                         <h3 style="color: #000;padding:5px;">${post.message} </h3>
-                        </strong>
+                        </strong></div>
                         </fieldset>
                     `);
                     }
+                    
                     postsContainer.append(postElement);
                 });
 
@@ -130,23 +131,11 @@ $(document).ready(function() {
             // Append the message to the chat window with the user's color
             const postElement = $('<div>').addClass('message');
             const userColor = getUserColor(username); // Get user's color based on name
-            if (storedUsername===post.name) {
+            if (storedUsername===username) {
                 // If username is not stored, prompt user to input their name
                 postElement.html(`
                 <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-left-radius: 10px;padding:5px;">
-                <legend style="color:#fff;background-color: ${userColor}; padding: 5px ;  border-top-left-radius: 10px; border-bottom-right-radius: 10px;">${username} <i class="fa fa-paper-plane"></i></legend>
-                
-                <div style="">
-                <strong>
-                <h3 style="color: #000;padding:5px;">${message} </h3>
-                </strong></div>
-                </fieldset>
-            `);
-            } else {
-                // If username is stored, use it to send messages
-                postElement.html(`
-                <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-right-radius: 10px;padding:5px;">
-                <legend style="color:#fff;background-color: ${userColor}; padding: 5px ;  border-top-left-radius: 10px; border-bottom-right-radius: 10px;">${username} <i class="fa fa-paper-plane"></i></legend>
+                <legend style="color:#fff;background-color: ${userColor}; padding: 5px ;  border-top-left-radius: 10px; border-bottom-right-radius: 10px;float:right;">${username} <i class="fa fa-paper-plane"></i></legend>
                 
                 <div style="">
                 <strong>
