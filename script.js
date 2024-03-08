afe37f477d6a4461586bbccfc6d389d9b579827e
Ali <alouloucheikh05@gmail.com>
2024-03-08 23:26:55 +0100

    wd

--- script.js
+++ script.js
@@ -45,7 +45,6 @@ $(document).ready(function() {
                         </fieldset>
                     `);
                     }
-                    
                     postsContainer.append(postElement);
                 });
 
