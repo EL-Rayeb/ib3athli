62f856124cfd7b8bfde45d0b94cd5f695b138a45
Ali <alouloucheikh05@gmail.com>
2024-03-10 13:41:07 +0100

    full on full update -adding sperate chat view for chat rooms -setting up index -new file placements -scroll down on load fix +ti5dim ya mnayyik

--- script.js
+++ script.js
@@ -1,70 +1,98 @@
 $(document).ready(function() {
-    // Function to fetch data from Google Sheets
-    function fetchData() {
-        fetch('https://script.google.com/macros/s/AKfycbzkUPuvLYnGs46DCLLoRZ4Ao0-i95nVP3_fuDA6mBxLPc2o0DjtkCvHwADLmsknNi_3/exec')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const postsContainer = $('#postsContainer');
-                const isScrolledToBottom = postsContainer[0].scrollHeight - postsContainer.scrollTop() === postsContainer.outerHeight();
+     // Function to fetch data from Google Sheets
+ function fetchData() {
+    fetch('https://script.google.com/macros/s/AKfycbzkUPuvLYnGs46DCLLoRZ4Ao0-i95nVP3_fuDA6mBxLPc2o0DjtkCvHwADLmsknNi_3/exec')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const postsContainer = $('#postsContainer');
+            const isScrolledToBottom = postsContainer[0].scrollHeight - postsContainer.scrollTop() === postsContainer.outerHeight();
 
-                // Clear previous messages
-                postsContainer.empty();
+            // Clear previous messages
+            postsContainer.empty();
 
-                // Iterate through the data array to create HTML elements
-                data.forEach(post => {
-                    const postElement = $('<div>').addClass('message');
-                    let userColor = getUserColor(post.name); // Get user's color based on name
-                    const storedUsername = localStorage.getItem('username');
-                    if (storedUsername===post.name) {
-                        // If username is not stored, prompt user to input their name
-                        postElement.html(`
-                        <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-left-radius: 10px;padding:5px; float: right; width:90%; margin-top:5px;margin-bottom:5px   ">
-                        <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-top-left-radius: 10px; border-bottom-right-radius: 10px; float:right;" >${post.name} <i class="fa fa-check-double"></i></legend>
-                        
-                        <div style="">
-                        <strong>
-                        <h3 style="color: #000;padding:5px;">${post.message} </h3>
-                        </strong></div>
-                        </fieldset>
-                    `);
-                    } else {
-                        // If username is stored, use it to send messages
-                        postElement.html(`
-                        <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-right-radius: 10px;padding:5px;display:block">
-                        <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-bottom-left-radius: 10px; border-top-right-radius: 10px;position:absolute;">${post.name} <i class="fa fa-check-double"></i></legend>
-                        
-                        <div style="margin-top:30px">
-                        <strong>
-                        <h3 style="color: #000;padding:5px;">${post.message} </h3>
-                        </strong></div>
-                        </fieldset>
-                    `);
-                    }
+            // Iterate through the data array to create HTML elements
+            data.forEach(post => {
+                const postElement = $('<div>').addClass('message');
+                let userColor = getUserColor(post.name); // Get user's color based on name
+                const storedUsername = localStorage.getItem('username');
+                if (storedUsername===post.name) {
+                    // If username is not stored, prompt user to input their name
+                    postElement.html(`
+                    <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-left-radius: 10px;padding:5px; float: right; width:90%; margin-top:5px;margin-bottom:5px   ">
+                    <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-top-left-radius: 10px; border-bottom-right-radius: 10px; float:right;" >${post.name} <i class="fa fa-check-double"></i></legend>
                     
-                    postsContainer.append(postElement);
-                });
-
-                // Scroll to the bottom if previously scrolled to the bottom
-                if (isScrolledToBottom) {
-                    postsContainer.scrollTop(postsContainer[0].scrollHeight);
+                    <div style="">
+                    <strong>
+                    <h3 style="color: #000;padding:5px;">${post.message} </h3>
+                    </strong></div>
+                    </fieldset>
+                `);
+                } else {
+                    // If username is stored, use it to send messages
+                    postElement.html(`
+                    <fieldset style=" border:5px solid ${userColor}; border-top-right-radius: 10px; border-top-left-radius: 10px; border-bottom-right-radius: 10px;padding:5px;display:block">
+                    <legend style="color:#fff;background-color: ${userColor}; padding: 5px ; border-bottom-left-radius: 10px; border-top-right-radius: 10px;position:absolute;">${post.name} <i class="fa fa-check-double"></i></legend>
+                    
+                    <div style="margin-top:30px">
+                    <strong>
+                    <h3 style="color: #000;padding:5px;">${post.message} </h3>
+                    </strong></div>
+                    </fieldset>
+                `);
                 }
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-                // Display an error message using SweetAlert2
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Connection Failure !',
-                    text: 'You are not online. Please try again later.'
-                });
+                
+                postsContainer.append(postElement);
             });
+
+            // Scroll to the bottom if previously scrolled to the bottom
+            if (isScrolledToBottom) {
+                postsContainer.scrollTop(postsContainer[0].scrollHeight);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            // Display an error message using SweetAlert2
+            Swal.fire({
+                icon: 'error',
+                title: 'Connection Failure !',
+                text: 'You are not online. Please try again later.'
+            });
+        });
+}
+    // Check if username is already stored in local storage
+    const storedUsername = localStorage.getItem('username');
+    if (!storedUsername) {
+        // If username is not stored, prompt user to input their name
+        promptForUsername();
+    } else {
+        // If username is stored, use it to send messages
+        sendMessage(storedUsername);
     }
 
+    // Function to prompt user for username
+    function promptForUsername() {
+        Swal.fire({
+            icon: 'warning',
+            title: 'username',
+            showConfirmButton:false,
+            showCancelButton: false,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Save username to local storage
+                localStorage.setItem('username', result.value);
+                // Send message with username
+                sendMessage(result.value);
+            }
+        });
+        console.log('Username prompt displayed.'); // Log when username prompt is displayed
+    }
     // Function to generate a color based on the hash code of the name
     function getUserColor(username) {
         // Generate hash code
@@ -79,40 +107,6 @@ $(document).ready(function() {
             color += ('00' + value.toString(16)).substr(-2);
         }
         return color;
-    }
-
-    // Check if username is already stored in local storage
-    const storedUsername = localStorage.getItem('username');
-    if (!storedUsername) {
-        // If username is not stored, prompt user to input their name
-        promptForUsername();
-    } else {
-        // If username is stored, use it to send messages
-        sendMessage(storedUsername);
-    }
-
-    // Function to prompt user for username
-    function promptForUsername() {
-        Swal.fire({
-            title: 'Enter your name',
-            input: 'text',
-            inputAttributes: {
-                required: 'required'
-            },
-            showCancelButton: false,
-            confirmButtonText: 'Submit',
-            allowOutsideClick: false,
-            allowEscapeKey: false,
-            allowEnterKey: true
-        }).then((result) => {
-            if (result.isConfirmed) {
-                // Save username to local storage
-                localStorage.setItem('username', result.value);
-                // Send message with username
-                sendMessage(result.value);
-            }
-        });
-        console.log('Username prompt displayed.'); // Log when username prompt is displayed
     }
 
     // Function to send message with username
@@ -168,7 +162,8 @@ $(document).ready(function() {
                 Swal.fire({
                     icon: 'error',
                     title: 'Message is not sent',
-                    text: 'Your internet connection is weak. Please try again later.'
+                    text: 'Your internet connection is weak. Please try again later.',
+                    text: 'Please try again later.'
                 });
             });
         });
@@ -183,11 +178,6 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop: $('#postsContainer')[0].scrollHeight }, 'slow');
     });
 
-    // Scroll to bottom on page load
-    $(window).on('load', function() {
-        $('#postsContainer').scrollTop($('#postsContainer')[0].scrollHeight);
-    });
-
     // Add scroll-to-bottom button functionality
     $('#scrollButton').click(function() {
         $('html, body').animate({ scrollTop: $('#postsContainer')[0].scrollHeight }, 'slow',);
@@ -198,7 +188,7 @@ $(document).ready(function() {
     $(window).scroll(function() {
         const $scrollButton = $('#scrollButton');
 
-        if ($(this).scrollTop() < 1000) {
+        if ($(this).scrollTop() < 100) {
             $scrollButton.fadeOut();
         } else {
             $scrollButton.fadeIn();
@@ -212,13 +202,37 @@ $(document).ready(function() {
         }
 
         // Show/hide scroll button based on scroll direction
-        if ($(this).scrollTop() < 1000 && $(this).scrollTop() > previousScroll) {
-            $messageForm.fadeIn();
+        if ($(this).scrollTop() < 100 && $(this).scrollTop() > previousScroll) {
+            $scrollButton.fadeIn();
         } else {
-            $messageForm.fadeOut();
+            $scrollButton.fadeOut();
         }
         previousScroll = ($(this).scrollTop());
     });
 
-    var previousScroll = 5;
+    var previousScroll = 1;
+
+    let scrollingDown = true; // Flag to track scrolling state
+
+    function scrollDown() {
+        if (scrollingDown) {
+            window.scrollBy(0, 15); // Scroll down by 15 pixels
+            requestAnimationFrame(scrollDown);
+        }
+    }
+    
+    // Start the infinite scroll
+    scrollDown();
+    
+    // Listen for scroll events
+    window.addEventListener("scroll", () => {
+        // Check if the user has scrolled up
+        if (window.scrollY < lastScrollY) {
+            // Stop the infinite scroll
+            scrollingDown = false;
+        }
+        lastScrollY = window.scrollY; // Update last scroll position
+    });
+    
+    let lastScrollY = window.scrollY; // Initialize last scroll position
 });
